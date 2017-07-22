58c7e6ac9a4f26c6d79981073607509e4bdb6e9c
gnoubi <travis@travis-ci.com>
2017-07-22 23:14:54 +0100

    gama sensit

--- gateway/GamaSenseIt.cpp
+++ gateway/GamaSenseIt.cpp
@@ -1,10 +1,64 @@
 
 #include "GamaSenseIt.h"
 
+
+
+
+
 using namespace std;
 
+GamaSenseIT::GamaSenseIT(SX1272 &loraConnection)
+{
+	string id = CLIENTID;
+	gatewayName = new char[id.length()+1];
+	strcpy(gatewayName,id.c_str());
+	this->loraConnector = loraConnection;
+	sensorName = new map<int,string>();
+	brokerAddress = ADDRESS;
+	useBroker = false;
+	saveInFile = false;
+	fileName = DEFAULT_FILE_NAME;
 
 
+}
+
+void GamaSenseIT::setupLora()
+{
+	int e;
+    // Print a start message
+    printf("loraConnector module and Raspberry Pi: receive packets with ACK\n");
+    
+    // Power ON the module
+    e = loraConnector.ON();
+    
+    delay(1000);
+    printf("Setting power ON: state %d\n", e);
+    
+    // Set transmission mode
+    e = loraConnector.setMode(4);
+    printf("Setting Mode: state %d\n", e);
+    // Set header
+    e = loraConnector.setHeaderON();
+    printf("Setting Header ON: state %d\n", e);
+    
+    // Select frequency channel
+    e = loraConnector.setChannel(CH_09_868);
+    printf("Setting Channel: state %d\n", e);
+    // Set CRC
+    e = loraConnector.setCRC_ON();
+    printf("Setting CRC ON: state %d\n", e);
+    
+    // Select output power (Max, High or Low)
+    e = loraConnector.setPower('H');
+    printf("Setting Power: state %d\n", e);
+    
+    // Set the node address
+    e = loraConnector.setNodeAddress(1);
+    printf("Setting Node address: state %d\n", e);
+    
+    // Print a success message
+    printf("loraConnector successfully configured\n\n");
+}
 
 bool GamaSenseIT::containPrefix(string s, string  prefix)
 {
@@ -20,52 +74,73 @@ bool GamaSenseIT::containPrefix(string s, string  prefix)
 
 void GamaSenseIT::sendToSensor(string data,int receiverAddress)
 {
+	int e;
     string prefix = GAMA_SENS_IT_MESSAGE_HEADER;
-    
+
     string toSend = prefix + data;
     char dtToSend[toSend.size()+1];//as 1 char space for null is also required
     strcpy(dtToSend, toSend.c_str());
-    e = sx1272.sendPacketTimeout(receiverAddress, dtToSend);
+
+
+#ifdef WITH_ACK
+  int n_retry=NB_RETRIES;
+
+  do {
+    e = sx1272.sendPacketTimeoutACKRetries(receiverAddress, dtToSend);
+
+    n_retry--;
+
+    if (n_retry == 0)
+        cout<<"Abort message to "<<receiverAddress<<endl;
+    	cout<<"contents "<<dtToSend<<endl;
+  } while (e && n_retry);
+#else
+  e = loraConnector.sendPacketTimeout(receiverAddress, dtToSend);
+#endif
 }
+
 string GamaSenseIT::extractData(string message)
 {
-    string prefix = GAMA_SENS_IT_MESSAGE_HEADER;
-    string result=message.substr(prefix.size());
-    return result;
+	string prefix = GAMA_SENS_IT_MESSAGE_HEADER;
+	string result=message.substr(prefix.size());
+	return result;
 }
 
 void GamaSenseIT::waitAndReceiveMessage(string& message, int& source)
 {
+	int e;
     boolean cc = true;
     int sender = 0;
     string prefix = GAMA_SENS_IT_MESSAGE_HEADER;
     string receivedMessage = "";
+    cout<<"waiting message."<<endl;
     do
     {
+    	e = 0;
         string tmpReceivedMessage = "";
-        e = sx1272.receivePacketTimeout(10000);
+        e = loraConnector.receivePacketTimeout(10000);
         if ( e == 0 )
         {
-            sender =sx1272.packet_received.src;
-            for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
+            sender =loraConnector.packet_received.src;
+            for (unsigned int i = 0; i < loraConnector.packet_received.length; i++)
             {
-                tmpReceivedMessage += (char)sx1272.packet_received.data[i];
+                tmpReceivedMessage += (char)loraConnector.packet_received.data[i];
             }
             if(containPrefix(tmpReceivedMessage,prefix))
             {
                 receivedMessage = extractData(tmpReceivedMessage);
-                
                 cc = false;
-            }
+             }
         }
         else {
-            cout<<".."<<endl;
+            cout<<".";
             // Serial.println(e, DEC);
         }
     }while(cc);
     message  =receivedMessage;
     source = sender;
-    
+    cout<<"receiving message from : "<<source<<endl;
+    cout<<"contents : "<<message<<endl;
     //return 0;
 }
 
@@ -78,6 +153,7 @@ unsigned long GamaSenseIT::getdate()
     return tt;
 }
 
+
 void GamaSenseIT::sendDate(int receiverAddress)
 {
     unsigned long mdate =getdate();
@@ -89,29 +165,27 @@ void GamaSenseIT::sendDate(int receiverAddress)
     sendToSensor(data,receiverAddress);
 }
 
-
-
 int GamaSenseIT::messageCommand(string message)
-{
-    int found = message.find(GAMA_SENS_IT_MESSAGE_CAPTURE_COMMAND);
-    if(found!=std::string::npos)
-        return CAPTURE_COMMAND;
-    found = message.find(GAMA_SENS_IT_MESSAGE_UPDATE_DATE_COMMAND);
-    if(found!=std::string::npos)
-        return DATE_UPDATE_COMMAND;
-    found = message.find(GAMA_SENS_IT_MESSAGE_REGISTER_COMMAND);
-    if(found!=std::string::npos)
-        return REGISTER_COMMAND;
-    return -1;
-}
+ {
+     int found = message.find(GAMA_SENS_IT_MESSAGE_CAPTURE_COMMAND);
+     if(found!=std::string::npos)
+         return CAPTURE_COMMAND;
+     found = message.find(GAMA_SENS_IT_MESSAGE_UPDATE_DATE_COMMAND);
+     if(found!=std::string::npos)
+         return DATE_UPDATE_COMMAND;
+     found = message.find(GAMA_SENS_IT_MESSAGE_REGISTER_COMMAND);
+     if(found!=std::string::npos)
+         return REGISTER_COMMAND;
+     return -1;
+ }
 
 int GamaSenseIT::offsetMessageContent(string message)
 {
     switch(messageCommand(message))
     {
-        case CAPTURE_COMMAND : { return sizeof(GAMA_SENS_IT_MESSAGE_CAPTURE_COMMAND);}
-        case DATE_UPDATE_COMMAND : { return sizeof(GAMA_SENS_IT_MESSAGE_UPDATE_DATE_COMMAND);}
-        case REGISTER_COMMAND : { return sizeof(GAMA_SENS_IT_MESSAGE_REGISTER_COMMAND);}
+        case CAPTURE_COMMAND : { return strlen(GAMA_SENS_IT_MESSAGE_CAPTURE_COMMAND);}
+        case DATE_UPDATE_COMMAND : { return strlen(GAMA_SENS_IT_MESSAGE_UPDATE_DATE_COMMAND);}
+        case REGISTER_COMMAND : { return strlen(GAMA_SENS_IT_MESSAGE_REGISTER_COMMAND);}
     }
     return -1;
 }
@@ -122,13 +196,31 @@ string GamaSenseIT::messageContents(string message)
     return tailString;
 }
 
+int GamaSenseIT::sendToBrocker(string message, string sender, int sensorDate)
+ {
+ 	string dte = std::to_string(sensorDate);
+ 	string data =""+ dte+";"+sender+";"+message;
+ 	char  msg[data.length() + 1];
+ 	strcpy(msg, data.c_str());
+ 	int rc;
+ 	pubmsg.payload = msg;
+     pubmsg.payloadlen = message.length();
+     pubmsg.qos = QOS;
+     pubmsg.retained = 0;
+     MQTTClient_publishMessage(client, gatewayName, &pubmsg, &token);
+     printf("Waiting for up to %d seconds for publication of %s\n"
+             "on topic %s for client with ClientID: %s\n",
+             (int)(TIMEOUT/1000), msg, gatewayName, CLIENTID);
+     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+     printf("Message with delivery token %d delivered\n", token);
+     return rc;
+ }
+
 int GamaSenseIT::computeCaptureCommand(string message, int senderAddress)
 {
     string datePrefix = GAMA_SENS_IT_MESSAGE_DATE;
     string valuePrefix = GAMA_SENS_IT_MESSAGE_VALUE;
-    
-    cout << "donnees arrivees"<<message<<endl;
-    
+
     int dateFound = message.find(GAMA_SENS_IT_MESSAGE_DATE);
     if(dateFound==std::string::npos)
         return -1;
@@ -136,66 +228,141 @@ int GamaSenseIT::computeCaptureCommand(string message, int senderAddress)
     if(dataFound==std::string::npos)
         return -1;
     int sensorDate;
-    
-    
+
+
     int dateIndex =dateFound + datePrefix.size();
     int dateSize =dataFound - dateIndex ;
     istringstream( message.substr(dateIndex,dateSize)) >> sensorDate;
     int dataIndex =dataFound + valuePrefix.size();
     string data = message.substr(dataIndex);
-    
-    cout<<"date:"<< sensorDate << endl<<"  data :"<<data<<endl;
-    return 0;
+    string ssender = sensorName->find(senderAddress)->second;
+    if(saveInFile == true)
+    {
+    	(*outFile) <<  sensorDate << ";"<<ssender<<";"<<data<<"\r\n";
+    	outFile->flush();
+    }
+    int sending = useBroker==false?0:sendToBrocker(data,  ssender,  sensorDate);
+    return sending;
 }
 
 void GamaSenseIT::computeRegisterCommand(string message, int senderAddress)
 {
     string senderName = messageContents(message);
+    cout <<"sender name" <<senderName <<endl;
     sensorName->insert(make_pair(senderAddress,senderName));
     sendDate(senderAddress);
 }
 
 void GamaSenseIT::computeMessage(string message, int senderAddress)
+ {
+     int command = messageCommand(message);
+     switch(command)
+     {
+         case CAPTURE_COMMAND : {
+             computeCaptureCommand(message,senderAddress);
+             break;
+         }
+         case REGISTER_COMMAND : {
+             computeRegisterCommand(message,senderAddress);
+             break;
+         }
+     }
+ }
+
+void GamaSenseIT::setupMQTT(string address, string clientID)
 {
-    int command = messageCommand(message);
-    
-    switch(command)
+	string pro = ADDRESS_PROTOCOL;
+	string port = ADDRESS_PORT;
+
+	char  tcpAddress[pro.length()+port.length()+1+address.length()];
+	strcpy(tcpAddress,pro.c_str());
+	strcat(tcpAddress,address.c_str());
+	strcat(tcpAddress,port.c_str());
+	int rc;
+
+	char id_c[clientID.length()+1];
+	strcpy(id_c,clientID.c_str());
+
+	 MQTTClient_create(&client, tcpAddress, id_c,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
     {
-        case CAPTURE_COMMAND : {
-            computeCaptureCommand(message,senderAddress);
-            break;
-        }
-        case REGISTER_COMMAND : {
-            computeRegisterCommand(message,senderAddress);
-            break;
-        }
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
     }
 }
 
-void setup()
+void GamaSenseIT::setupOutFile(string outf)
 {
-   // setupLora();
+    outFile = new ofstream();
+    outFile->open(outf.c_str());
 }
 
-void GamaSenseIT::waitAndCompute(string& msg, int& source)
+void  GamaSenseIT::analyseParameter(string cmd, string value)
 {
-        waitAndReceiveMessage(msg,source);
-        computeMessage(msg,source);
+	if (cmd.compare("-broker") == 0)
+	{
+		useBroker = true;
+		brokerAddress = value;
+	}
+	if (cmd.compare("-name") == 0)
+	{
+		gatewayName = new char[value.length()+1];
+		strcpy(gatewayName, value.c_str());
+	}
+	if (cmd.compare("-file") == 0)
+	{
+		saveInFile = true;
+		fileName = value;
+	}
 }
 
-GamaSenseIT::GamaSenseIT(SX1272 & loraConnection)
+
+void GamaSenseIT::setup()
 {
-    sx1272 = loraConnection;
-    this->sensorName = new map<int,string>();
-    this->e = 0;
-    
+    setupLora();
+    if(useBroker == true)
+    {
+        setupMQTT(brokerAddress,gatewayName);
+    }
+    if(saveInFile == true)
+    {
+    	setupOutFile(fileName);
+    }
+
+
 }
+
+void GamaSenseIT::loop()
+{
+   while(true)
+   {
+       string msg = "";
+       int source = -1;
+       waitAndReceiveMessage(msg,source);
+       computeMessage(msg,source);
+    }
+}
+
+
 
 int main(int argc, char *argv[])
 {
-    setup();
-    string msg;
-    int sender;
-    //waitAndCompute(msg,sender);
+	gamaSenseIt = new GamaSenseIT(sx1272);
+
+	for(int i = 1; i+1<argc; i=i+2 )
+	{
+		string cmd(argv[i]);
+		string val(argv[i+1]);
+		cout <<"command: "<<cmd<<" value: "<<val<<endl;
+		gamaSenseIt->analyseParameter(cmd,val);
+	}
+
+	gamaSenseIt->setup();
+	gamaSenseIt->loop();
     return 0;
 }
+
--- gateway/GamaSenseIt.h
+++ gateway/GamaSenseIt.h
@@ -1,7 +1,7 @@
 #ifndef GAMA_SENSE_IT
 #define GAMA_SENSE_IT
+
 #include "SX1272.h"
-#include "Message.h"
 #include <unistd.h>
 
 #include <iostream>
@@ -9,9 +9,14 @@
 #include <stdlib.h>
 #include <string>
 #include <time.h>
+#include <fstream>
 #include <sstream>
 #include <map>
 #include <cstring>
+#include "MQTTClient.h"
+
+#define NB_RETRIES 10
+#define WITH_ACK
 
 #define GAMA_SENS_IT_MESSAGE_HEADER "GamaSenseIT_"
 #define GAMA_SENS_IT_MESSAGE_UPDATE_DATE_COMMAND "UPDATE_DATE_"
@@ -19,8 +24,17 @@
 #define GAMA_SENS_IT_MESSAGE_CAPTURE_COMMAND "CAPTURE"
 #define GAMA_SENS_IT_MESSAGE_DATE "_DATE_"
 #define GAMA_SENS_IT_MESSAGE_VALUE "_VALUES_"
+#define CAPTURE_COMMAND 1
+#define DATE_UPDATE_COMMAND 2
+#define REGISTER_COMMAND 3
 
-
+#define ADDRESS     "192.168.1.71"
+#define ADDRESS_PROTOCOL     "tcp://"
+#define ADDRESS_PORT     ":1883"
+#define CLIENTID    "GATEWAY"
+#define DEFAULT_FILE_NAME "sensorLogFile.csv"
+#define QOS         1
+#define TIMEOUT     10000L
 
 using namespace std;
 
@@ -29,31 +43,49 @@ using namespace std;
 class GamaSenseIT
 {
 private:
-    SX1272 sx1272;
-    map<int,string>* sensorName;
-    int e;
+
+	SX1272 loraConnector;
+	map<int,string>* sensorName;
+	MQTTClient client;
+	MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+	MQTTClient_message pubmsg = MQTTClient_message_initializer;
+	MQTTClient_deliveryToken token;
+
+	ofstream * outFile;
+	char* gatewayName ;
+	string brokerAddress;
+	bool useBroker;
+	bool saveInFile;
+	string fileName;
+
     
-    bool containPrefix(string s, string prefix);
+    void setupLora();
+    bool containPrefix(string s, string  prefix);
     string extractData(string message);
     int messageCommand(string message);
     int offsetMessageContent(string message);
     string messageContents(string message);
+    int sendToBrocker(string message, string sender, int sensorDate);
     int computeCaptureCommand(string message, int senderAddress);
     void computeRegisterCommand(string message, int senderAddress);
-    void waitAndReceiveMessage(string& message, int& source);
     void computeMessage(string message, int senderAddress);
-    
-    
-    
+    void setupMQTT(string address, string clientID);
+    void setupOutFile(string outf);
+
+
+
 public:
     GamaSenseIT(SX1272 &loraConnection);
     
     void sendToSensor(string data,int receiverAddress);
+    void analyseParameter(string cmd, string value);
+    void waitAndReceiveMessage(string& message, int& source);
     void sendDate(int receiverAddress);
-    void waitAndCompute(string& msg, int& source);
     unsigned long getdate();
-    
-    
+    void setup();
+    void loop();
 };
+
+extern GamaSenseIT* gamaSenseIt;
 
 #endif
