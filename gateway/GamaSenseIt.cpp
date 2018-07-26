55ddf1d58b921f6917978e4c5dd448558567e359
nicolas <nicolas.marilleau@ird.fr>
2018-07-27 00:44:56 +0200

    gateway upgrade

--- gateway/GamaSenseIt.cpp
+++ gateway/GamaSenseIt.cpp
@@ -225,12 +225,12 @@ int GamaSenseIT::sendToBrocker(string message, string sender, string mid, unsign
 	data.append(mid);
 	data.append(";");
 	data.append(message);
- 	char  msg[data.size() ];
+ 	char  msg[data.size() +1];
  	strcpy(msg, data.c_str());
 	cout<<"message.  e "<<msg<<endl;
  	int rc;
  	pubmsg.payload = msg;
-     pubmsg.payloadlen = data.size();
+     pubmsg.payloadlen = data.size()+1;
      pubmsg.qos = QOS;
      pubmsg.retained = 0;
      MQTTClient_publishMessage(client, gatewayName, &pubmsg, &token);
