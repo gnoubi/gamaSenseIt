ae929edfaa263161d38b3b864c542913c4ace2d6
Jerome B <jeromebonacchi@orange.fr>
2019-06-18 12:54:42 +0200

    clean qameleo

--- website/website/src/app/pages/qr-qameleo/qr-qameleo.component.ts
+++ website/website/src/app/pages/qr-qameleo/qr-qameleo.component.ts
@@ -4,8 +4,6 @@ import { Router } from '@angular/router';
 
 import * as jQuery from 'jquery';
 
-import { sensorVersionService } from '../sensor-version/sensor-version-service'
-
 @Component({
   selector: 'qr-qameleo',
   templateUrl: './qr-qameleo.component.html',
@@ -22,8 +20,7 @@ export class QrQameleoComponent implements OnInit {
   private test;
   private url = 'http://vmpams.ird.fr:8080';
 
-  constructor( private sensor: sensorVersionService,
-    private router: Router,
+  constructor( private router: Router,
     private http: HttpClient ) { }
 
   ngOnInit() {
@@ -43,25 +40,6 @@ export class QrQameleoComponent implements OnInit {
         this.humidity = Math.round(this.humidity*100)/100;
       });
   }
-
-  // setValueFromURL(url: string){
-  //   var $ajax = jQuery.ajax({
-  //     type: 'GET',
-  //     url: url,
-  //     data: '',
-  //     async: false,
-  //     dataType: 'json',
-  //     success: function(data) {
-  //     }
-  //   });
-  //   this.PM1 = $ajax.responseJSON.pm1/2;
-  //   this.PM25 = $ajax.responseJSON.pm25/2;
-  //   this.PM10 = $ajax.responseJSON.pm10/2;
-  //   this.temperature = $ajax.responseJSON.temperature;
-  //   this.temperature = Math.round(this.temperature*100)/100;
-  //   this.humidity = $ajax.responseJSON.humidity;
-  //   this.humidity = Math.round(this.humidity*100)/100;
-  // }
 
   ngOnChanges(changes: SimpleChanges){
     this.PM1 = changes[this.PM1].currentValue;
