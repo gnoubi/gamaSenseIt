ab02d2153c766dffdd163487b92fd06b992d6d87
Jerome B <jeromebonacchi@orange.fr>
2019-06-19 14:44:06 +0200

    page and service for qrcode (not working yet)

---
+++ website/website/src/app/pages/qrcode/qrcode-reader.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class QrcodeReaderService {
+
+  qrcode: any;
+
+  decode(file: any): Observable<string> {
+    return new Observable(observer => {
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (e: any) => {
+        const data = e.target.result;
+        this.qrcode.callback = (res) => {
+          observer.next(res);
+          observer.complete();
+        };
+        this.qrcode.decode(data);
+      };
+
+    });
+  }
+}
--- website/website/src/app/pages/qrcode/qrcode.component.ts
+++ website/website/src/app/pages/qrcode/qrcode.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { QrcodeReaderService } from './qrcode-reader.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-qrcode',
   templateUrl: './qrcode.component.html',
   styleUrls: ['./qrcode.component.css']
 })
-export class QrcodeComponent implements OnInit {
+export class QrcodeComponent implements OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
 
-  ngOnInit() {
+  constructor(private qrReader: QrcodeReaderService) { }
+
+  ngOnDestroy(): void {
+    // this.subscription.unsubscribe();
+    console.log('ngOnDestroy actif');
   }
 
-  onFileChange() {
-    
+  onFileChange(event) {
+    const file = event.target.files[0];
+    this.subscription = this.qrReader.decode(file)
+      .subscribe(url => {
+        window.open(url, '_blank'); // '_self'
+      });
   }
 }
